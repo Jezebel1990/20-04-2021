683983971e7a3e2d20551e61a21f38036a4d5c5a
Jezebel1990 <75287031+Jezebel1990@users.noreply.github.com>
2021-04-20 23:46:31 -0300

    Atividade de banco de dados relacional Mysql

--- mySQL/atividade_escola.sql
+++ mySQL/atividade_escola.sql
@@ -1,4 +1,28 @@
-SELECT * FROM tb_alune;
+create database db_escola;
+use db_escola;
+
+create table tb_alune (
+ra int(4) AUTO_INCREMENT,
+nome varchar(30) NOT NULL,
+turma int NOT NULL,
+idade int NOT NULL,
+nota int NOT NULL,
+PRIMARY KEY (ra)
+);
+
+INSERT INTO tb_alune (ra, nome, turma, idade, nota) VALUES (NULL, 'Maria',2, 10, 8);
+INSERT INTO tb_alune  (ra, nome, turma, idade, nota) VALUES (NULL, 'João',3, 11, 9);
+INSERT INTO tb_alune (ra, nome, turma, idade, nota) VALUES (NULL, 'Guilherme',1, 9, 7);
+INSERT INTO tb_alune (ra, nome, turma, idade, nota) VALUES (NULL, 'Felipe',2, 10, 5);
+INSERT INTO tb_alune (ra, nome, turma, idade, nota) VALUES (NULL, 'Joana',3, 11, 8);
+INSERT INTO tb_alune (ra, nome, turma, idade, nota) VALUES (NULL, 'Edna',1, 8, 10);
+INSERT INTO tb_alune (ra, nome, turma, idade, nota) VALUES (NULL, 'Geralda',2, 10, 6);
+INSERT INTO tb_alune (ra, nome, turma, idade, nota) VALUES (NULL, 'Mario',3, 11, 9);
+
+SELECT * FROM tb_alune where nota >7;
+SELECT * FROM tb_alune where nota <7;
+UPDATE tb_alune set nome = "Edna Silva" where ra = NULL
+
 
 
 
